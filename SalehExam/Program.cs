a06aee09a3337f1ad97398291e672f7849945b98
Barbara18279 <Student@S215-1>
2021-04-26 14:22:55 +0300

    AAA

--- SalehExam/Program.cs
+++ SalehExam/Program.cs
@@ -10,6 +10,21 @@ namespace SalehExam
     {
         static void Main(string[] args)
         {
+
+            int b; // b - число,на которое мы будем делить
+            int result = 0; // сумма наших чисел
+            Console.Write("Введите b: ");
+            b = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % b == 0) // если остаток от деления i на b = 0, то суммируем
+                    result += i;
+            }
+
+            Console.WriteLine($"Сумма натуральных чисел кратных {b} и < 100 = {result}");
+            Console.ReadLine();
+
         }
     }
 }
